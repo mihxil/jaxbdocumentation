94cd63570ba96a83a2c6c583fe919254c6d738e7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-10 19:59:03 +0200

    Moved XmlDocumentation to package more similar to the other jaxb bind annotations

--- src/test/java/org/meeuw/jaxbdocumentation/DocumentationAdderTest.java
+++ src/test/java/org/meeuw/jaxbdocumentation/DocumentationAdderTest.java
@@ -13,6 +13,7 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamResult;
 
 import org.junit.Test;
+import org.meeuw.xml.bind.annotation.XmlDocumentation;
 import org.xml.sax.SAXException;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
