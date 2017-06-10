f849bf59df5534d126e33f8d16a2f503eb3857b1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-10 20:16:24 +0200

    Support for xs:simpleType

--- src/test/java/org/meeuw/jaxbdocumentation/DocumentationAdderTest.java
+++ src/test/java/org/meeuw/jaxbdocumentation/DocumentationAdderTest.java
@@ -51,6 +51,10 @@ public class DocumentationAdderTest {
         @XmlDocumentation(value = "some docu of element b in a")
         B b;
 
+        @XmlElement
+        @XmlDocumentation("Documentation for enum element")
+        SomeEnum someEnum;
+
         @XmlElements(
             @XmlElement(type = C.class)
         )
@@ -88,6 +92,15 @@ public class DocumentationAdderTest {
 
     }
 
+    @XmlType(namespace = NS)
+    @XmlDocumentation("Documentation about some enum")
+    public enum SomeEnum {
+        x,
+        y,
+        z
+
+    }
+
     @Test
     public void addDocumentation() throws JAXBException, IOException, SAXException, TransformerException, ParserConfigurationException {
         DocumentationAdder collector = new DocumentationAdder(A.class);
@@ -107,6 +120,11 @@ public class DocumentationAdderTest {
             "            <xs:element form=\"qualified\" minOccurs=\"0\" name=\"b\" type=\"tns:b\">\n" +
             "                <xs:annotation>\n" +
             "                    <xs:documentation>some docu of element b in a</xs:documentation>\n" +
+            "                </xs:annotation>\n" +
+            "            </xs:element>\n" +
+            "            <xs:element minOccurs=\"0\" name=\"someEnum\" type=\"tns:someEnum\">\n" +
+            "                <xs:annotation>\n" +
+            "                    <xs:documentation>Documentation for enum element</xs:documentation>\n" +
             "                </xs:annotation>\n" +
             "            </xs:element>\n" +
             "            <xs:element maxOccurs=\"unbounded\" minOccurs=\"0\" name=\"list\" type=\"tns:c\">\n" +
@@ -143,6 +161,16 @@ public class DocumentationAdderTest {
             "        </xs:annotation>\n" +
             "        <xs:sequence/>\n" +
             "    </xs:complexType>\n" +
+            "    <xs:simpleType name=\"someEnum\">\n" +
+            "        <xs:annotation>\n" +
+            "            <xs:documentation>Documentation about some enum</xs:documentation>\n" +
+            "        </xs:annotation>\n" +
+            "        <xs:restriction base=\"xs:string\">\n" +
+            "            <xs:enumeration value=\"x\"/>\n" +
+            "            <xs:enumeration value=\"y\"/>\n" +
+            "            <xs:enumeration value=\"z\"/>\n" +
+            "        </xs:restriction>\n" +
+            "    </xs:simpleType>\n" +
             "</xs:schema>");
 
     }
